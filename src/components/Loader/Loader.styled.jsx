2458e5ca4c0ab7d7ce32621fa4f3fdc0eb9002a4
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-11-12 16:50:25 +0200

    fix

--- src/components/Loader/Loader.styled.jsx
+++ src/components/Loader/Loader.styled.jsx
@@ -1,20 +1,2 @@
-import styled from 'styled-components';
-import { ThreeDots } from 'react-loader-spinner';
-
-export const Loader = styled(ThreeDots)`
-    height:"80"
-    width="80"
-    radius="9"
-    color={p => p.theme.colors.blue}
-    ariaLabel="three-dots-loading"
-    wrapperStyle={{}}
-    wrapperClassName=""
-    visible={true}
-`
-
-
-() => (
-  <ThreeDots
-    
-  />
-);
+// import styled from 'styled-components';
+// import { ThreeDots } from 'react-loader-spinner';
